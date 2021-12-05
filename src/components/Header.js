df631945e4de3bbedf1096a61576f9295c50cadb
Albert Lu <socerball30@gmail.com>
2021-12-04 20:28:18 -0500

    Turned off CI for build script
    And added small fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar, Button, Grid } from "@material-ui/core";
+import { AppBar, Toolbar, Button} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import {
